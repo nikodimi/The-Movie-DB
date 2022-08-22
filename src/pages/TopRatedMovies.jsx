68f450b1f80ce0145009323cfbfb714c247a98bd
nddesigns <56118523+nikodimi@users.noreply.github.com>
2022-08-22 19:28:41 +0200

    Displaying top rated movies

--- src/pages/TopRatedMovies.jsx
+++ src/pages/TopRatedMovies.jsx
@@ -1,10 +1,36 @@
 import Container from 'react-bootstrap/Container'
+import Row from 'react-bootstrap/Row'
+import Col from 'react-bootstrap/Col'
+import { useQuery } from 'react-query'
+import { getTopRatedMovies} from '../services/tmdbAPI'
+
 
 const TopRatedMovies = () => {
+    const { data: movies, isError, isLoading, isSuccess } = useQuery('movies', getTopRatedMovies)
+
 	return (
-		<Container className="py-3">
-			<h1>Top rated movies!</h1>
-		</Container>
+        <Container className="py-3">
+            <h1 className="mb-3">Top rated Movies!</h1>
+
+            {isLoading && (<p>Loading</p>)}
+
+            {isError && (<p>An error occurred!</p>)}
+
+            <div>
+                {isSuccess && (
+                    <>
+                        <Row>
+                            {movies.results.map((movie) => (
+                                <Col lg={3} md={4} sm={12} key={movie.id}>
+                                    <h4>{movie.title}</h4>
+                                    <p>{movie.release_date}</p>
+                                </Col>
+                            ))}
+                        </Row>
+                    </>
+                )}
+            </div>
+        </Container>
 	)
 }
 
