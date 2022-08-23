3c3a86cfd853d44b7c0bbd4c8fda0872cbbb485e
nikodimi <nikola.dimitrijoski@medieinstitutet.se>
2022-08-23 16:20:07 +0200

    Displaying the movie image

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,9 +1,10 @@
 import Card from 'react-bootstrap/Card'
 
 const MovieCard = ({ movie }) => {
+    const base_url = 'https://image.tmdb.org/t/p/w500'
     return (
         <Card className="mb-3">
-            {/* <Card.Img variant="top" src="holder.js/100px180" /> */}
+            <Card.Img variant="top" src={base_url + movie.poster_path} />
             <Card.Body>
                 <Card.Title><strong>Title: {movie.title}</strong></Card.Title>
                 <Card.Text>Release date: {movie.release_date}</Card.Text>
