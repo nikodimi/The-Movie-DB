d631916186efc175be7dc6804c302d1f67846cf8
nddesigns <56118523+nikodimi@users.noreply.github.com>
2022-08-27 15:03:52 +0200

    Added a footer

---
+++ src/components/Footer.jsx
@@ -0,0 +1,21 @@
+import Container from 'react-bootstrap/Container'
+import Row from 'react-bootstrap/Row'
+import Col from 'react-bootstrap/Col'
+
+const Footer = () => {
+    return (
+
+            <Container fluid className='footer'>
+                <Row>
+                    <Col>
+                        <div className="footer-content d-flex justify-content-center py-3">
+                            <p className='m-0'>Copyright © 2022 - All right reserved by Nisse</p>
+                        </div>
+                    </Col>
+                </Row>
+            </Container>
+
+    )
+}
+
+export default Footer
