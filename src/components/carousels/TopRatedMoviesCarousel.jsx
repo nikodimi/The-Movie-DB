921b02b522ae6ce6353947fa428f6ea58c40446a
nddesigns <56118523+nikodimi@users.noreply.github.com>
2022-08-29 10:56:28 +0200

    Added carousels to the home page

---
+++ src/components/carousels/TopRatedMoviesCarousel.jsx
@@ -0,0 +1,68 @@
+import Carousel from 'react-bootstrap/Carousel';
+import Row from 'react-bootstrap/Row'
+import Col from 'react-bootstrap/Col'
+import reel from '../../assets/images/film-reel.jpg'
+import Loading from '../Loading'
+import WarningAlert from '../alerts/WarningAlert'
+import useTopRatedMovies from '../../hooks/useTopRatedMovies'
+
+const TopRatedMoviesCarousel = () => {
+    const { data: movies, error, isError, isLoading, isSuccess } = useTopRatedMovies()
+    const base_url = 'https://image.tmdb.org/t/p/w500'
+
+    return (
+        <Row className='mb-5'>
+            <Col sm={12} >
+                        
+                <h2 className="mb-3 page-header">Top rated</h2>
+
+                {isLoading &&  <Loading />}
+
+                {isError && <WarningAlert message={error.message} />}
+
+                {isSuccess && (
+
+                    <Carousel>
+                        {movies.results.map((movie) => (
+                            <Carousel.Item key={movie.id}>
+                                <Row className='d-flex'>
+                                    <Col lg={3} md={4}sm={10} xs={10} className="m-auto">
+                                        <div className='img-wrapper'>
+                                            <img className="img-fluid single-movie-img" src={movie?.poster_path ? base_url + movie.poster_path : reel} alt="" />
+                                        </div>
+                                    </Col>
+                                    <Col lg={9} md={8} sm={10} xs={10} className="m-auto">
+                                        <div className='movie-info h-100 d-flex flex-column justify-content-between'>
+                                            <div>
+                                                <h2>{movie.title}</h2>
+                                            </div>
+                                            <div>
+                                                <h5>Release date</h5>
+                                                <p>{movie.release_date}</p>
+                                            </div>
+                                            <div>
+                                                <h5>Rating</h5>
+                                                <p>{Math.round(movie.vote_average * 10) / 10}</p>
+                                            </div>
+                                            {/* <div>
+                                                <h5>Genre</h5>
+
+                                            </div> */}
+                                            <div>
+                                                <h5>Plot</h5>
+                                                <p>{movie.overview}</p>    
+                                            </div>
+                                        </div>
+                                    </Col>
+                                </Row>    
+                            </Carousel.Item>
+                        ))}
+                    </Carousel>
+                )}
+            </Col>
+        </Row>
+
+    )
+}
+
+export default TopRatedMoviesCarousel
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,10 +1,29 @@
 import Container from 'react-bootstrap/Container'
+import NowPlayingCarousel from '../components/carousels/NowPlayingCarousel'
+import PopularMoviesCarousel from '../components/carousels/PopularMoviesCarousel'
+import TopRatedMoviesCarousel from '../components/carousels/TopRatedMoviesCarousel'
 
 const HomePage = () => {
-	return (
+    
+
+    return (
 		<Container className="py-3">
-			<h1>Welcome!</h1>
-		</Container>
+
+            <h2 className="mb-3 page-header py-3">Popular</h2>
+
+            <PopularMoviesCarousel/>
+
+            <hr />
+
+            <TopRatedMoviesCarousel />
+
+            <hr />
+
+            <h2 className="mb-3 page-header py-3">Now Playing</h2>
+
+            <NowPlayingCarousel />
+
+        </Container>
 	)
 }
 
