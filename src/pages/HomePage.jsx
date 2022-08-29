0342c5ef03cfac8271160d94f64ac6a6bae09e71
nddesigns <56118523+nikodimi@users.noreply.github.com>
2022-08-29 18:28:21 +0200

    Added a hero-banner to Home page

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,4 +1,6 @@
 import Container from 'react-bootstrap/Container'
+import Row from 'react-bootstrap/Row'
+import Col from 'react-bootstrap/Col'
 import NowPlayingCarousel from '../components/carousels/NowPlayingCarousel'
 import PopularMoviesCarousel from '../components/carousels/PopularMoviesCarousel'
 import TopRatedMoviesCarousel from '../components/carousels/TopRatedMoviesCarousel'
@@ -7,25 +9,42 @@ const HomePage = () => {
     
 
     return (
-		<Container className="py-3">
 
-            <h2 className="mb-3 page-header py-3">Popular Movies</h2>
+        <>
+            <Container fluid className='p-0 hero-banner'>
+                <Row>
+                    <Col>
+                        <div class="hero-image d-flex justify-content-center align-items-center">
+                            <div class="hero-text">
+                                <h1>Welcome.</h1>
+                                <h4>Millions of movies, TV shows and people to discover. Explore now.</h4>
+                            </div>
+                        </div>
+                    </Col>
+                </Row>
+                
+            </Container>
 
-            <PopularMoviesCarousel/>
+            <Container className="py-3">
 
-            <hr />
+                <h2 className="mb-3 page-header py-3">Popular Movies</h2>
 
-            <h2 className="mb-3 page-header py-3">Top Rated Movies</h2>
+                <PopularMoviesCarousel/>
 
-            <TopRatedMoviesCarousel />
+                <hr />
 
-            <hr />
+                <h2 className="mb-3 page-header py-3">Top Rated Movies</h2>
 
-            <h2 className="mb-3 page-header py-3">Now Playing Movies</h2>
+                <TopRatedMoviesCarousel />
 
-            <NowPlayingCarousel />
+                <hr />
 
-        </Container>
+                <h2 className="mb-3 page-header py-3">Now Playing Movies</h2>
+
+                <NowPlayingCarousel />
+
+            </Container>
+        </>
 	)
 }
 
